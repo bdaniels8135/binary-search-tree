d9f5cb9f5eb0cfe0793fca375a8cd4d0373d02aa
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-03 18:49:19 -0500

    Copy prettyprint function to a module

---
+++ prettyPrintBST.mjs
@@ -0,0 +1,12 @@
+export default function prettyPrintBST(node, prefix = "", isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrintBST(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrintBST(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+}
