54846e22a511580607d781cb6755d69c78cf67e8
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-03 22:04:20 -0500

    Implement insertion and deletion with helpers

--- BinarySearchTree.mjs
+++ BinarySearchTree.mjs
@@ -8,10 +8,10 @@ export default class BinarySearchTree {
 
   constructor(array) {
     this.#data = [...new Set(array)].sort((a, b) => a - b);
-    this.#root = this.buildTree(this.#data);
+    this.#root = this.#buildTree(this.#data);
   }
 
-  buildTree(array, startIndex = 0, endIndex = array.length - 1) {
+  #buildTree(array, startIndex = 0, endIndex = array.length - 1) {
     if (startIndex > endIndex) return null;
     const midIndex = Math.floor((startIndex + endIndex) / 2);
     const newTNode = new TNode(array[midIndex]);
@@ -24,9 +24,62 @@ export default class BinarySearchTree {
     prettyPrintBST(this.#root);
   }
 
-  insert(value) {}
+  #locateNode(value) {
+    let parent;
+    let side;
+    let node = this.#root;
+    while (node !== null) {
+      if (node.value === value) break;
+      parent = node;
+      side = node.value < value ? "right" : "left";
+      if (side === "right") node = node.right;
+      else node = node.left;
+    }
+    return [parent, node, side];
+  }
 
-  delete(value) {}
+  insert(value) {
+    const [parent, node, side] = this.#locateNode(value);
+    if (node != null && node.value === value) return this;
+    const newTNode = new TNode(value);
+    if (parent == null) this.#root = newTNode;
+    else if (side === "right") parent.right = newTNode;
+    else parent.left = newTNode;
+    return this;
+  }
+
+  #deleteNode(nodeLocation) {
+    const [parent, node, side] = nodeLocation;
+    if (node == null) return null;
+    if (node.left == null && node.right == null) {
+      if (side === "right") parent.right = null;
+      else parent.left = null;
+      return node;
+    }
+    if (node.left != null && node.right != null) {
+      let prevNode = node;
+      let nextNode = node.right;
+      let nextNodeSide = "right";
+      while (nextNode.left != null) {
+        nextNodeSide = "left";
+        prevNode = nextNode;
+        nextNode = nextNode.left;
+      }
+      const nextNodeLocation = [prevNode, nextNode, nextNodeSide];
+      this.#deleteNode(nextNodeLocation);
+      node.value = nextNode.value;
+      return node;
+    }
+    if (node.right != null) parent[side] = node.right;
+    else parent[side] = node.left;
+    return node;
+  }
+
+  delete(value) {
+    const nodeLocation = this.#locateNode(value);
+    this.#deleteNode(nodeLocation);
+    return this;
+  }
 
   find(value) {}
 
@@ -47,9 +100,18 @@ export default class BinarySearchTree {
   rebalance() {}
 }
 
-const data = [...Array(20)].map(() => Math.floor(Math.random() * 100));
+// const randomArray = [...Array(20)].map(() => Math.floor(Math.random() * 100));
 
-const bst = new BinarySearchTree(data);
+const constArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const bst = new BinarySearchTree(constArray);
+
+bst.insert(10).insert(9).insert(7000).delete(7).delete(9).delete(4);
 
 bst.prettyPrint();
-console.log(data);
+
+const emptyBST = new BinarySearchTree();
+
+emptyBST.insert(1);
+
+emptyBST.prettyPrint();
